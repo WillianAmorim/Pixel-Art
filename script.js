f93b82eafd84d069563ac5724b1aa448751382f0
Willian <willian.amorim1@outlook.com>
2023-01-07 21:55:41 -0300

    criado função que faz os pixels

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 document.getElementById('black').classList.add('selected');
+
+const idSectionPixel = document.getElementById('pixel-board');
+
+const createPixels = (number) => {
+
+  for (let i = 0; i < number; i++) {
+    const tagDiv = document.createElement('div');
+    
+    idSectionPixel.appendChild(tagDiv);
+
+    for (let c = 0; c < number; c++) {
+      const tagDivChild = document.createElement('div');
+      tagDivChild.classList.add('pixel')
+      tagDiv.appendChild(tagDivChild)
+    }
+  }
+};
+
+createPixels(5)
