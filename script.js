3ba20c3e8627df4d17e613149f745a8fa7e6b0b1
Willian <willian.amorim1@outlook.com>
2023-01-07 22:12:43 -0300

    criado função que altera a classe selected para quem for clicado na paleta

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const createPixels = (number) => {
 
   for (let i = 0; i < number; i++) {
     const tagDiv = document.createElement('div');
-    
+
     idSectionPixel.appendChild(tagDiv);
 
     for (let c = 0; c < number; c++) {
@@ -18,3 +18,24 @@ const createPixels = (number) => {
 };
 
 createPixels(5)
+
+//CLICK NAS PALETAS
+const palettes = document.querySelectorAll('.color');
+
+let corSelecionada = '';
+
+const selectedColor = (event) => {
+  if(!event.target.classList.contains('selected')) {
+    for (let i = 0; i < palettes.length; i++) {
+      if (palettes[i].classList.contains('selected')) {
+        palettes[i].classList.remove('selected');
+      }
+    }
+    event.target.classList.add('selected');
+    corSelecionada = event.target.id;
+  }
+}
+
+for (let i = 0; i < palettes.length; i++) {
+  palettes[i].addEventListener('click', selectedColor)
+}
